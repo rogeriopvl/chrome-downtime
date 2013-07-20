66371860f195131e73f1983bf9668bdc3026537c
rogeriopvl <rogeriopvl@gmail.com>
2013-07-20 01:26:53 +0100

    First prototype working. Needs stop button.

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,1 +1,53 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var downtime = Downtime();
+    var button = document.getElementById('start_button');
+    button.addEventListener('click', downtime.start);
+});
+
+var Downtime = function() {
+    var timer;
+
+    var start = function(duration) {
+        setProxy();
+    };
+
+    var stop = function() {
+        unsetProxy();
+    };
+
+    var setProxy = function() {
+        var config = {
+            mode: 'fixed_servers',
+            rules: {
+                proxyForHttp: {
+                    scheme: 'socks5',
+                    host: '1.2.3.4'
+                },
+                // bypassList: ['foobar.com']
+            }
+        };
+        chrome.proxy.settings.set({
+            value: config,
+            scope: 'regular'
+        }, function() {
+            console.log('Proxy set.');
+        });
+    };
+
+    var unsetProxy = function() {
+        chrome.proxy.settings.set({
+            value: { mode: 'direct' },
+            scope: 'regular'
+        }, function() {
+            console.log('Proxy resetted.');
+        });
+    };
+
+    return {
+        start: start,
+        stop: stop
+    }
+
+};
